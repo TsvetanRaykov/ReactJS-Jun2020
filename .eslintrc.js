bc6452476fedb3146172a849621b6c110786fe7f
Tsvetan Raykov <tsvetan.raykov@semansys.com>
2020-07-28 18:48:40 +0300

    feat: Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ['react-app', 'prettier'
+    // 'plugin:react/recommended',
+    // 'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'printWidth': 80,
+        'trailingComma': 'es5',
+        'semi': false,
+        'jsxSingleQuote': true,
+        'singleQuote': true,
+        'useTabs': true,
+        "endOfLine": "auto"
+      }
+    ]
+  }
+}
